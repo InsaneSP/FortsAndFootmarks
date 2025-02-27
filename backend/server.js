302022425be93725b6d81c3f4f92a2a241e7c17b
unknown <smitpotkar20@gmail.com>
2025-02-27 16:08:02 +0530

    Updated cors for railways testing

--- backend/server.js
+++ backend/server.js
@@ -10,8 +10,6 @@ const userRoutes = require("./routes/userRoutes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const cors = require("cors");
-
 app.use(cors({
     origin: "https://forts-and-footmarks.vercel.app",
     credentials: true,
